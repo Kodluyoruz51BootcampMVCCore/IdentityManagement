b7d13fa08ba71b6fd02124825e8c84ef9e545fea
Hakkı Sağdıç <hakkisagdic@gmail.com>
2020-06-27 12:52:47 +0300

    project infra and web api project added.

---
+++ IdentityManagement.Infrastructure/Persistence/AppIdentityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityManagement.Infrastructure.Persistence
+{
+    public class AppIdentityDbContext : IdentityDbContext
+    {
+        public AppIdentityDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AppRole>().HasData(new AppRole() { Name = "user" });
+            base.OnModelCreating(builder);
+        }
+    }
+}
